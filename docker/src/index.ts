37650f9c23b2f535ff2660356638a7561243944f
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2024-05-21 16:12:41 +0530

    commit

--- docker/src/index.ts
+++ docker/src/index.ts
@@ -1,29 +1,37 @@
-
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 app.use(express.json());
 
 const client = new PrismaClient();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later"
+});
+
+app.use(limiter);
+
 app.get("/", (req, res) => {
-    res.json({
-        message: "Healthy server"
-    })
-})
+  res.json({
+    message: "Healthy server"
+  });
+});
 
 app.post("/", async (req, res) => {
-    await client.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name
-        }
-    })
+  await client.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name
+    }
+  });
 
-    res.json({
-        message: "Done signing up!"
-    })
-})
+  res.json({
+    message: "Done signing up!"
+  });
+});
 
 app.listen(3000);
